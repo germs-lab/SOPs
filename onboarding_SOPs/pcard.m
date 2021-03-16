95bf5aa63983c00d731062abeb2f13c419711dfb
Adina Howe <adina@users.noreply.github.com>
2021-03-16 14:15:04 -0500

    Create pcard.m

---
+++ onboarding_SOPs/pcard.m
@@ -0,0 +1,7 @@
+* Go to Workday --> Applications Desktop --> Requests
+* Actions --> Create Request
+* Type "p-card" and select "Card Application - Procurement"
+* Fill in form and submit 
+* Describe the request:  Procurement card for buying materials and supplies for graduate research
+* Our address:  1201 Sukup Hall, Ames, IA 50011
+* Graduate students choose:  $5000 Limit and $3000 single purchase.
